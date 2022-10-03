4a52ffe17f78f74608df41ccd1e6a0e875c46e6d
Alexsandro <alexsandroalves321@hotmail.com>
2022-10-03 20:38:21 -0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const connection = require("./database/database")
 
 // View engine
 app.set('view engine','ejs');
@@ -9,8 +10,19 @@ app.set('view engine','ejs');
 app.use(express.static('public'));
 
 //Body parser
-app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+// database
+
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("Success Conection")
+    }).catch((error) => {
+        console.log(error);
+    })
+
 
 app.get("/", (req,res) => {
     res.render("index");
