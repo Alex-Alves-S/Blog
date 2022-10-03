e2e7326c30906db70573907e29560cd7876f3b0f
Alexsandro <alexsandroalves321@hotmail.com>
2022-10-03 19:06:44 -0300

    upp

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+// View engine
+app.set('view engine','ejs');
+
+// Static
+app.use(express.static('public'));
+
+//Body parser
+app.use(bodyParser.urlencoded({extends: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req,res) => {
-    res.send("Bem vindo ao meu site");
+    res.render("index");
 })
+
+
 
 app.listen(8080, () =>{
     console.log("O servidor está rodando ...");
