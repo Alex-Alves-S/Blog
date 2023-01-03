dd0a7a975fa54d6f42b171792644232ab7bf04d6
Alex-Alves-S <alexsandroavles321@hotmail.com>
2023-01-03 02:31:11 -0300

    up

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/admin/categories/new", (req,res) =>{
-    res.send("Rota de categoria")
+    res.render("admin/categories/new");
 });
 
 
